0702feac72c19273dc34f9781eeb2c883c6c8c08
缘 <111369757+b8l8u8e8@users.noreply.github.com>
2022-12-07 14:44:56 +0800

    Add files via upload

---
+++ 切片.py
@@ -0,0 +1,16 @@
+# 开发时间：2022/12/6  22:39
+# 对lst进行切片
+lst=[0,1,2,3,4,5,6]
+print(lst)
+lst2=lst[::-1]
+print(lst2)
+#tuple
+tp=(0,1,2,3,4,5,6)
+print(tp)
+tp2=tp[2::2]
+print(tp2)
+#str
+st="01234567"
+print(st)
+st2=st[1:5:1]
+print(st2)
--- 字符串操作.py
+++ 字符串操作.py
@@ -1,10 +1,17 @@
-# 开发时间：2022/10/5  23:09
-#字符串分割
-
-lst="hello world".split()
-print(lst)
-lst="hello|world".split("|")
-print(lst)
-#替换
-print("hello python python python".replace("python","java",2))
-
+# 开发时间：2022/12/6  19:59
+a="itheimahl"
+# 字符串替换 得到新的字符串替换，原有字符串没有修改
+b=a.replace("i",str(1),1)
+print(f"字符串替换后为{b}")
+# 字符串分割 返回值
+b=a.split("h",1)
+print(b)
+#字符串的规整操作（去除最前最后指定字符--若是字符串分开处理）
+a="12356123621"
+print(a.strip("12"))
+# 统计字符出现次数
+b=a.count("1")
+print(f"i出现的次数是{b}")
+# 字符串长度
+b=len(a)
+print(f"字符串a的长度为{b}")
